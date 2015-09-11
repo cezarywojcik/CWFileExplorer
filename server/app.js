5f06e0301056060994e04d64b3c438d0813f04f7
Cezary Wojcik <cezarywojcik92@gmail.com>
2015-09-11 10:43:35 -0700

    add node server

---
+++ server/app.js
@@ -0,0 +1,34 @@
+/**
+ * File: app.js
+ * Date: Sep 11, 2015 10:23:20
+ * Desc: The main file for the CWFileExplorer server.
+ * Auth: Cezary Wojcik
+ */
+
+// ---- [ includes ] -----------------------------------------------------------
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var jade = require("jade");
+
+// ---- [ setup ] --------------------------------------------------------------
+
+var app = express();
+app.engine("jade", jade.__express);
+app.set("view engine", "jade");
+app.set("view options", {
+    layout: false
+});
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser());
+
+// ---- [ routing ] ------------------------------------------------------------
+
+
+
+// ---- [ run server ] --------------------------------------------------------
+
+var server = app.listen(3000, function() {
+  console.log("server running");
+});
---
+++ server/public/main.js
@@ -0,0 +1,29 @@
+/**
+ * File: main.js
+ * Date: Sep 11, 2015 10:36:26
+ * Desc: The main JavaScript file for CWFileExplorer.
+ * Auth: Cezary Wojcik
+ */
+
+function getDocument(url) {
+  var templateXHR = new XMLHttpRequest();
+  templateXHR.responseType = "document";
+  templateXHR.addEventListener("load", function() {pushDoc(templateXHR.responseXML);}, false);
+  templateXHR.open("GET", url, true);
+  templateXHR.send();
+  return templateXHR;
+}
+ 
+function pushDoc(document) {
+  navigationDocument.pushDocument(document);
+}
+ 
+App.onLaunch = function(options) {
+  var templateURL = 'http://localhost:3000/test.tvml';
+  getDocument(templateURL);
+}
+ 
+App.onExit = function() {
+  console.log('App finished');
+}
+
